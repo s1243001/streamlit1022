b9c9a72299ed714e7b72df8046439fe9a035a962
s1243001 <s1243001@gm.ncue.edu.tw>
2025-10-22 11:38:28 +0800

    Revise home page title and introduction text
    Updated the title and content for the home page.

--- page_home.py
+++ page_home.py
@@ -1,11 +1,4 @@
 import streamlit as st
-# ó¿¿öw
-st.title("Ïsö GIS \ÿ")
-st.write("o/[o Streamlit ö×_Wo_")
-#  MP4 ö URL ó st.video()
-video_url = "https://i.imgur.com/1GoAB0C.mp4"
-st.write(f"kÿ {video_url}")
-st.video(video_url)
-#  wö URL ó st.image()
-image_url = "https://i.imgur.com/uf1T4ND.png"
-st.image(image_url)
+st.title("吳承翰")
+st.header("吳承翰的GIS專題")
+st.write("一段簡短的自我介紹，以及這個網頁的用途")
